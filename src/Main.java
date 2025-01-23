bd22f2a0861bbc55ef1288355edc6bf81e553d46
chalshik <nurmuhammedkalmamatov06@gmail.com>
2025-01-23 14:39:23 +0600

    first task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello");
+        List<Integer> list  = List.of(1,2,3,3,4);
+        Map<String, Integer> New = FindAvr(list);
+        System.out.println(New);
     }
+    public static  Map<String, Integer> FindAvr(List<Integer> numbers){
+        Map<String,Integer> slovar = new HashMap<>();
+        int sum = 0;
+        int len = 0;
+        for ( int number:numbers){
+            sum += number;
+            len +=1;
+        }
+        int Max = Collections.max(numbers);
+        int Min = Collections.min(numbers);
+        slovar.put("Sum",sum);
+        slovar.put("Avr",sum/len);
+        slovar.put("max",Max);
+        slovar.put("min",Min);
+        return slovar;
+    }
+
 }
